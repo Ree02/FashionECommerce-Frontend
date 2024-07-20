b123dc10191ac9095f430761af715d20dcde9b63
kinjo <ri.02kh.aax@gmail.com>
2024-07-21 01:00:57 +0900

    :art:カラーの指定方法を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Top from "./components/pages/Top.tsx";
 import Login from "./components/pages/Login.tsx";
 
-const App = () => {
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#889C9B",
+        },
+        secondary: {
+            main: "#B2BEBF",
+        },
+        error: {
+            main: "#BD2A2E",
+        },
+        success: {
+            main: "#486966",
+        },
+    },
+});
+
+export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" Component={Top} />
-                <Route path="/login" Component={Login} />
-            </Routes>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Routes>
+                    <Route path="/" Component={Top} />
+                    <Route path="/login" Component={Login} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
     );
-};
-export default App;
+}
--- src/components/functions/colors.tsx
+++ src/components/functions/colors.tsx
@@ -1,5 +1,2 @@
-export const FontColor: string = "#3B3936";
-export const BackgroundColor: string = "#B2BEBF";
-export const MainColor: string = "#889C9B";
-export const CollectColor: string = "#486966";
-export const ErrorColor: string = "#BD2A2E";
+export const DarkFontColor: string = "#3B3936";
+export const LightFontColor: string = "#fff";
